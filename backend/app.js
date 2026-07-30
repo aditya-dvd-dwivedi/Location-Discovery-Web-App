8af492bf2d9fe5a825158a64741d662e46257f0d
aditya-dvd-dwivedi <ad7xx9019@gmail.com>
2026-07-30 11:31:01 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -65,7 +65,7 @@ app.use((error,req,res,next)=>{
 mongoose
     .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kquazjj.mongodb.net/${process.env.DB_NAME}?appName=Cluster0`)
     .then(()=>{
-        app.listen(5000);
+        app.listen(process.env.PORT||5000);
     })
     .catch(err =>{
         console.log(err);
